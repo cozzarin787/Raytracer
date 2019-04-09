d75d89cfd1ae6b4222aa6eb36fc00c7efb074cef
Pablo Ordorica <pxo2297@rit.edu>
2019-04-09 17:05:29 -0400

    Commented out calculation furthest intersection point in Sphere intersect

--- Raytracer/Sphere.cpp
+++ Raytracer/Sphere.cpp
@@ -34,10 +34,9 @@ Object::intersectResult Sphere::intersect(Ray r)
 	float omega;
 	float xi, yi, zi;
 	
-	Point intersectPoints[2];
 	Point closest;
-	Point furthest;
-	RowVector3f normals[2];
+	//Point furthest;
+	RowVector3f normal;
 
 	if (roots < 0)
 	{
@@ -82,19 +81,19 @@ Object::intersectResult Sphere::intersect(Ray r)
 				if (temp < omega && temp > epsilon)
 				{
 					omega = temp;
-					furthest = Point(closest.x, closest.y, closest.z);
+					//furthest = Point(closest.x, closest.y, closest.z);
 					xi = x0 + dx * omega;
 					yi = y0 + dy * omega;
 					zi = z0 + dz * omega;
 					closest = Point(xi, yi, zi);
 				}
-				else
-				{
-					xi = x0 + dx * omega;
-					yi = y0 + dy * omega;
-					zi = z0 + dz * omega;
-					furthest = Point(xi, yi, zi);
-				}
+				//else
+				//{
+				//	xi = x0 + dx * omega;
+				//	yi = y0 + dy * omega;
+				//	zi = z0 + dz * omega;
+				//	furthest = Point(xi, yi, zi);
+				//}
 			}
 		}
 		if (omega < 0)
@@ -107,15 +106,9 @@ Object::intersectResult Sphere::intersect(Ray r)
 		}
 
 	}
-	intersectPoints[0] = closest;
-	intersectPoints[1] = furthest;
-	for (Point p : intersectPoints)
-	{
-		int i = 0;
-		normals[i++] = RowVector3f(xi - xc, yi - yc, zi - zc).normalized();
-	}
+	normal = RowVector3f(xi - xc, yi - yc, zi - zc).normalized();
 	
-	return intersectResult(true, omega, this->mat, intersectPoints[0], normals[0]);
+	return intersectResult(true, omega, this->mat, closest, normal);
 }
 
 void Sphere::transform(Matrix<float, 4, 4, RowMajor> transMat)
