9ce927f826b3e2c6cce45e0b6489a8cfef0cd6ce
Jacob Cozzarin <cozzarin787@gmail.com>
2019-05-04 18:59:13 -0400

    create default constructor for Ray

--- Raytracer/Ray.cpp
+++ Raytracer/Ray.cpp
@@ -1,5 +1,11 @@
 #include "Ray.h"
 
+Ray::Ray()
+{
+	this->origin = Point(0, 0, 0);
+	this->direction = RowVector3f(1, 1, 1);
+}
+
 Ray::Ray(Point origin, RowVector3f direction)
 {
 	this->origin = origin;
